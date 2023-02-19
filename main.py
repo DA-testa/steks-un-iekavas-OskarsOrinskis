86e2b0ca798bc3cf898f83db376d090494f6e4bf
OskarsOrinskis <125825846+OskarsOrinskis@users.noreply.github.com>
2023-02-20 00:44:30 +0200

    m

--- main.py
+++ main.py
@@ -11,20 +11,61 @@ def are_matching(left, right):
 
 def find_mismatch(text):
     opening_brackets_stack = []
-    for i, next in enumerate(text):
-        if next in "([{":
+    for i, char in enumerate(text):
+        
+        if char in "([{":
+            
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(char,i))
+        
+          
 
-        if next in ")]}":
+        if char in ")]}":
             # Process closing bracket, write your code here
-            pass
+            
+            if not opening_brackets_stack:
+                
+                return f"{i+1}"
+        
+        else: 
 
+            top = opening_brackets_stack.pop()
+            
+            if not are_matching(top.char, char):
+                
+                return f"{i+1}"
+    
+    
+    if opening_brackets_stack:
+        
+        top = opening_brackets_stack.pop()
+        
+        return F"(top.position+1)"    
+    
+    return "Success"
+        
 
 def main():
     text = input()
+    
+    if "I" in text:
+        
+        text = input()
+        
+    
+
+
     mismatch = find_mismatch(text)
-    # Printing answer, write your code here
+    
+    if not mismatch:
+        
+        point("Success")
+        
+    else:
+    
+    
+    print(mismatch)
+    
 
 
 if __name__ == "__main__":
