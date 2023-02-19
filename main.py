a24e1805e353c0084e4d547d740455a23082ed1e
OskarsOrinskis <125825846+OskarsOrinskis@users.noreply.github.com>
2023-02-20 01:25:42 +0200

    kkk

--- main.py
+++ main.py
@@ -32,13 +32,7 @@ def find_mismatch(text):
             
     
     
-    if opening_brackets_stack:
-        
-        return  opening_brackets_stack[-1].position
-        
-            
-    
-    return "Success"
+  
         
 
 def main():
@@ -49,7 +43,6 @@ def main():
         text = input()
         
     
-
 
         mismatch = find_mismatch(text)
     
